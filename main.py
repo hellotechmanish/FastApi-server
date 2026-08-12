5a54520767a5c64d59d6b24deb75b3ba1493a56b
Manish Kumar <mk772542@gmail.com>
2026-08-12 23:51:54 +0530

    first deploy

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "FastAPI server is running"
+    }
+
+
+@app.get("/health")
+def health():
+    return {
+        "status": "ok"
+    }
